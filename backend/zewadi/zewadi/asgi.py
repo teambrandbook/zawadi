95764dc20e88ced34db0ae1c59495a83317af43e
Hiran-sudheer <gethiranonline@gmail.com>
2026-05-26 17:01:18 +0530

    fix:prod bug fix

--- backend/zewadi/zewadi/asgi.py
+++ backend/zewadi/zewadi/asgi.py
@@ -1,14 +1,15 @@
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zewadi.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 import notifications.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zewadi.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
